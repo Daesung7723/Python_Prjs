a4e34b72368f7155d9926414560cfae8875855d4
daesung7723 <daesung7723@gmail.com>
2024-08-07 01:51:32 +0900

    2nd_day mission

--- 01_C_Python/03_gTTS.py
+++ 01_C_Python/03_gTTS.py
@@ -3,8 +3,8 @@ from playsound import playsound
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-text = "one two three four"
+text = "현수씨는 바보"
 
-tts = gTTS(text=text, lang='en')
+tts = gTTS(text=text, lang='ko')
 tts.save("hi.mp3")
 playsound("hi.mp3")
