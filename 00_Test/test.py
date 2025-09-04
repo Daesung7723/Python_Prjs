c9bb498406044515e096cb294c4b1b53bd3faca4
Daesung7723 <daesung7723@gmail.com>
2025-09-04 10:56:43 +0900

    0904_update-2

--- 00_Test/test.py
+++ 00_Test/test.py
@@ -1,37 +1,8 @@
-from machine import Pin, PWM
-import utime
+# 5부터 1까지 카운트다운하는 예시
+count = 5
 
-# Init Buzzer PWM
-buz = PWM(Pin(13))
-buz.duty_u16(int(65536/2)) # Duty = 50%
+while count > 0:  # count가 0보다 큰 동안 계속 반복
+    print(f"카운트다운: {count}")
+    count = count - 1  # count 값을 1씩 줄여서 언젠가는 조건이 False가 되게 만듭니다.
 
-Note_Freqs = {'Do_0':523, 'Re_0':587, 'Mi_0':659, 'Fa_0':698, \
-              'Sol_0':783, 'La_0':880, 'Ti_0':987, 'Do_1':1046,\
-              'Re_1':1174, 'Mi_1':1318, 'Fa_1':1397, 'Sol_1':1568,\
-              'La_1':1760, 'Ti_0':1975, 'Do_2':2093}
-
-Twingkle_Little_Star = [['Do_0',0.5], ['Do_0',0.5], ['Sol_0',0.5], ['Sol_0',0.5], \
-                        ['La_0',0.5], ['La_0',0.5], ['Sol_0',1], \
-                        ['Fa_0',0.5], ['Fa_0',0.5], ['Mi_0',0.5], ['Mi_0',0.5],\
-                        ['Re_0',0.5], ['Re_0',0.5], ['Do_0',1],\
-                        ['Sol_0',0.5],['Sol_0',0.5],['Fa_0',0.5],['Fa_0',0.5],\
-                        ['Mi_0',0.5], ['Mi_0',0.5], ['Re_0',1],\
-                        ['Sol_0',0.5],['Sol_0',0.5],['Fa_0',0.5],['Fa_0',0.5],\
-                        ['Mi_0',0.5], ['Mi_0',0.5], ['Re_0',1],\
-                        ['Do_0',0.5], ['Do_0',0.5], ['Sol_0',0.5], ['Sol_0',0.5], \
-                        ['La_0',0.5], ['La_0',0.5], ['Sol_0',1], \
-                        ['Fa_0',0.5], ['Fa_0',0.5], ['Mi_0',0.5], ['Mi_0',0.5],\
-                        ['Re_0',0.5], ['Re_0',0.5], ['Do_0',1]]
-
-def pNote(freq, len):
-    buz.freq(freq)
-    utime.sleep(len)
-    buz.deinit()
-    utime.sleep(0.02)
-
-def pMusic(music):
-    for note, l in music:
-        pNote(Note_Freqs[note], l)
-    
-if __name__ == '__main__' :
-    pMusic(Twingkle_Little_Star)
+print("발사!")
